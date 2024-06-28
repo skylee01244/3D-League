b4e88f661874fa39eccfe1fed6c3af727dda6936
skylee01244 <skylee01244@gmail.com>
2024-06-28 18:06:16 +1000

    Add project files.

---
+++ rpg-game/main.cpp
@@ -0,0 +1,43 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "RPG Game");
+    window.setFramerateLimit(60);
+
+    sf::RectangleShape rect;
+    sf::Vector2f rectanglePosition(600, 350);
+    sf::Vector2f rectangleSize(100, 100);
+
+    rect.setPosition(rectanglePosition);
+    rect.setSize(rectangleSize);
+
+    float xVelocity = 3;
+    float yVelocity = 3;
+
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) { window.close(); }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) { window.close(); }
+        }
+        //out of bounds
+        if (rectanglePosition.x < 0 || rectanglePosition.x > 1280-100) { xVelocity *= -1; }
+        if (rectanglePosition.y < 0 || rectanglePosition.y > 720-100) { yVelocity *= -1; }
+
+        //physics
+        rectanglePosition.x += xVelocity;
+        rectanglePosition.y += yVelocity;
+        rect.setPosition(rectanglePosition);
+
+        //render
+        window.clear();
+        window.draw(rect);
+
+        window.display();
+    }
+
+    return 0;
+}
