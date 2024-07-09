01d55886233636adfd830d837dd311f6294acce4
skylee01244 <skylee01244@gmail.com>
2024-07-10 05:16:40 +1000

    Updated the Main.cpp file + edited the FrateRate function

--- rpg-game/main.cpp
+++ rpg-game/main.cpp
@@ -15,6 +15,8 @@ int main()
 
     std::chrono::microseconds lag(0);   // make the game framerate-independent
 
+    std::chrono::steady_clock::time_point previous_time;
+
     sf::Event event;
 
     sf::ContextSettings settings;
@@ -33,10 +35,10 @@ int main()
     map_wall_texture.loadFromFile("Resources/Images/MapWall" + std::to_string(MAP_CELL_SIZE) + ".png");
 
     Player player(0, 0);
+    Enemy enemy(0, 0);
+	FrameRate frameRate;
 
-    Steven steven(0, 0);
-
-    map = convert_sketch(player, steven);
+    map = convert_sketch(player, enemy);
 
     map_grid_cell_sprite.setTexture(map_grid_cell_texture);
     map_grid_cell_sprite.setTextureRect(sf::IntRect(0, 0, MAP_GRID_CELL_SIZE, MAP_GRID_CELL_SIZE));
@@ -51,30 +53,69 @@ int main()
         sf::Time deltaTimeTimer = clock.restart();
         double deltaTime = deltaTimeTimer.asMicroseconds() / 1000.0; // You can also just do 1 / seconds
         double fps = 1.0f / deltaTimeTimer.asSeconds(); 
-
-        //-----------------------------------UPDATE-------------------------------------------
-        while (window.pollEvent(event)) 
-        {
-            if (event.type == sf::Event::Closed) { window.close(); }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) { window.close(); }
-        }
-
-        sf::Vector2f mousePosition = sf::Vector2f(sf::Mouse::getPosition(window));
-
         frameRate.Update(deltaTime, fps);
-        enemy.Update(deltaTime);
-        player.Update(deltaTime, enemy, mousePosition);
-        player2D.Update(deltaTime, enemy, mousePosition);
-        //-----------------------------------UPDATE-------------------------------------------
-        
-        //-----------------------------------DRAW-------------------------------------------
-        window.clear(sf::Color::Black);
-        enemy.Draw(window);
-        player.Draw(window);
-        player2D.Draw(window);
         frameRate.Draw(window);
-        window.display();
-        //-----------------------------------DRAW-------------------------------------------
+
+		std::chrono::microseconds delta_time = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - previous_time);
+
+		lag += delta_time;
+
+		previous_time += delta_time;
+
+		while (FRAME_DURATION <= lag)
+		{
+			lag -= FRAME_DURATION;
+
+			while (1 == window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed) { window.close(); }
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) { window.close(); }
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::H)) { draw_map = 1 - draw_map; }
+			}
+
+			player.update(map, window);
+
+			enemy.update(map, window);
+
+			if (FRAME_DURATION > lag)
+			{
+				window.clear(sf::Color(73, 255, 255));		// Colour the sky
+
+				player.draw_screen(window, enemy);
+
+				if (1 == draw_map)
+				{
+					for (unsigned short a = 0; a < ceil(MAP_CELL_SIZE * MAP_WIDTH / static_cast<float>(MAP_GRID_CELL_SIZE)); a++)
+					{
+						for (unsigned short b = 0; b < ceil(MAP_CELL_SIZE * MAP_HEIGHT / static_cast<float>(MAP_GRID_CELL_SIZE)); b++)
+						{
+							map_grid_cell_sprite.setPosition(static_cast<float>(MAP_GRID_CELL_SIZE * a), static_cast<float>(MAP_GRID_CELL_SIZE * b));
+
+							window.draw(map_grid_cell_sprite);
+						}
+					}
+
+					for (unsigned short a = 0; a < MAP_WIDTH; a++)
+					{
+						for (unsigned short b = 0; b < MAP_HEIGHT; b++)
+						{
+							if (Cell::Wall == map[a][b])
+							{
+								map_wall_sprite.setPosition(static_cast<float>(MAP_CELL_SIZE * a), static_cast<float>(MAP_CELL_SIZE * b));
+
+								window.draw(map_wall_sprite);
+							}
+						}
+					}
+
+					player.draw_map(window);
+
+					enemy.draw_map(window);
+				}
+
+				window.display();
+			}
+		}
     }
 
     return 0;
