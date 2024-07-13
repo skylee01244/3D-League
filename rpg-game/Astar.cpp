af543cdaea6a74fbd5de28b16a8ba84aa6249a3b
skylee01244 <skylee01244@gmail.com>
2024-07-14 03:52:03 +1000

    Moved get_adjacent_cells function into Astar.cpp

--- rpg-game/Astar.cpp
+++ rpg-game/Astar.cpp
@@ -4,10 +4,13 @@
 #include <SFML/Graphics.hpp>
 
 #include "Headers/Global.h"
-#include "Headers/GetAdjacentCells.h"
+#include "Headers/GetCell.h"
 
-//Guess what? I made a whole video explaining how the A star algorithm works along with Dijkstra and Breadth-first search.
-//Here it is: https://youtu.be/W1qvb86YOK0
+//Every cell has a f value(f = g + h)
+//g = Distance From start
+//h = hypothetical distance from cell to finish
+
+std::vector<sf::Vector2<unsigned short>> get_adjacent_cells(const sf::Vector2<unsigned short>& i_cell, const gbl::MAP::Map<gbl::MAP::PathCell>& i_map);
 
 float calculate_h_score(const sf::Vector2<unsigned short>& i_cell_0, const sf::Vector2<unsigned short>& i_cell_1)
 {
@@ -19,7 +22,6 @@ float calculate_h_score(const sf::Vector2<unsigned short>& i_cell_0, const sf::V
 	return std::max(distance_x, distance_y) + std::min(distance_x, distance_y) * (sqrt(2) - 1);
 }
 
-//SO MANY ARGUMENTS!!!!
 void astar_reset(unsigned short& i_path_length, std::map<gbl::Position<>, gbl::Position<>>& i_previous_cell, std::vector<sf::Vector2<unsigned short>>& i_path_vector, gbl::MAP::Map<float>& i_f_scores, gbl::MAP::Map<float>& i_g_scores, gbl::MAP::Map<float>& i_h_scores, const sf::Vector2<unsigned short>& i_finish_position, const sf::Vector2<unsigned short>& i_start_position, gbl::MAP::Map<gbl::MAP::PathCell>& i_map)
 {
 	i_path_length = 0;
@@ -139,4 +141,51 @@ void astar_search(unsigned short& i_path_length, std::map<gbl::Position<>, gbl::
 			}
 		}
 	}
+}
+
+std::vector<sf::Vector2<unsigned short>> get_adjacent_cells(const sf::Vector2<unsigned short>& i_cell, const gbl::MAP::Map<gbl::MAP::PathCell>& i_map)
+{
+	std::array<bool, 9> valid_adjacent_cells = {};
+
+	std::vector<sf::Vector2<unsigned short>> adjacent_cells;
+	std::vector<sf::Vector2<unsigned short>> diagonal_adjacent_cells;
+
+	for (unsigned char a = 0; a < valid_adjacent_cells.size(); a++)
+	{
+		gbl::MAP::PathCell cell = get_cell(sf::Vector2<short>(i_cell.x + a % 3 - 1, i_cell.y + floor(a / 3.f) - 1), i_map);
+
+		valid_adjacent_cells[a] = gbl::MAP::PathCell::Invalid != cell && gbl::MAP::PathCell::Wall != cell;
+	}
+
+	for (unsigned char a = 0; a < 3; a++)
+	{
+		for (unsigned char b = 0; b < 3; b++)
+		{
+			if ((1 != a || 1 != b) && 1 == valid_adjacent_cells[b + 3 * a])
+			{
+				if (abs(a - 1) == abs(b - 1))
+				{
+					//When a potential adjacent cell is located diagonally, we need to check 2 additional cells as well.
+					if (1 == valid_adjacent_cells[1 + 3 * a] && 1 == valid_adjacent_cells[3 + b])
+					{
+						diagonal_adjacent_cells.push_back(sf::Vector2<unsigned short>(b + i_cell.x - 1, a + i_cell.y - 1));
+					}
+				}
+				else
+				{
+					adjacent_cells.push_back(sf::Vector2<unsigned short>(b + i_cell.x - 1, a + i_cell.y - 1));
+				}
+			}
+		}
+	}
+
+	//I just realized that we don't need to put diagonal adjacent cells separately since we're not using the BFS algorithm.
+	//I wish I could fix it but I don't want to.
+	//...
+	//Why don't you fix it instead, huh?
+	//Yeah, this will be your homework.
+	//I'm your teacher now.
+	adjacent_cells.insert(adjacent_cells.end(), diagonal_adjacent_cells.begin(), diagonal_adjacent_cells.end());
+
+	return adjacent_cells;
 }
--- rpg-game/Headers/GetAdjacentCells.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-std::vector<sf::Vector2<unsigned short>> get_adjacent_cells(const sf::Vector2<unsigned short>& i_cell, const gbl::MAP::Map<gbl::MAP::PathCell>& i_map);
