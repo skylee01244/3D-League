2c495023b4f1681d1490298c2b2b043e192373c5
skylee01244 <skylee01244@gmail.com>
2024-07-30 21:18:38 +1000

    Updated all the code from previous errors

--- rpg-game/Headers/AngleFunctions.h
+++ rpg-game/Headers/AngleFunctions.h
@@ -1,12 +1,5 @@
 #pragma once
 
-//float deg_difference(const float i_angle_0, const float i_angle_1);
-//float deg_to_rad(const float i_degrees);
-//float get_degrees(const float i_degrees);
-//float get_radians(const float i_radians);
-//float rad_difference(const float i_angle_0, const float i_angle_1);
-//float rad_to_deg(const float i_radians);
-
 float normalize_degrees(float degrees);
 float normalize_radians(float radians);
 float degrees_to_radians(float degrees);
