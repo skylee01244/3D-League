857bc42a6508ba20da06c1623a7abf10f5486494
skylee01244 <skylee01244@gmail.com>
2025-03-13 05:09:16 +1100

    Updated Astar (i) variable names

--- rpg-game/Headers/Astar.h
+++ rpg-game/Headers/Astar.h
@@ -7,3 +7,5 @@ float calculate_h_score(const sf::Vector2<unsigned short>& i_cell0, const sf::Ve
 void astar_reset(unsigned short& i_path_length, std::map<gbl::Position<>, gbl::Position<>>& i_previous_cell, std::vector<sf::Vector2<unsigned short>>& i_path_vector, gbl::MAP::Map<float>& i_f_scores, gbl::MAP::Map<float>& i_g_scores, gbl::MAP::Map<float>& i_h_scores, const sf::Vector2<unsigned short>& i_finish_position, const sf::Vector2<unsigned short>& i_start_position, gbl::MAP::Map<gbl::MAP::PathCell>& i_map);
 
 void astar_search(unsigned short& i_path_length, std::map<gbl::Position<>, gbl::Position<>>& i_previous_cell, std::vector<sf::Vector2<unsigned short>>& i_path_vector, gbl::MAP::Map<float>& i_f_scores, gbl::MAP::Map<float>& i_g_scores, const gbl::MAP::Map<float>& i_h_scores, sf::Vector2<unsigned short>& i_next_cell, const sf::Vector2<unsigned short>& i_finish_position, const sf::Vector2<unsigned short>& i_start_position, gbl::MAP::Map<gbl::MAP::PathCell>& i_map);
+
+std::vector<sf::Vector2<unsigned short>> get_adjacent_cells(const sf::Vector2<unsigned short>& currentCell, const gbl::MAP::Map<gbl::MAP::PathCell>& map);
