8cf9298b4e4a7d468e80a10b00015d65ac318eb6
skylee01244 <skylee01244@gmail.com>
2025-03-13 04:32:16 +1100

    Updated variable names

--- rpg-game/Headers/Astar.h
+++ rpg-game/Headers/Astar.h
@@ -2,7 +2,7 @@
 #include "Global.h"
 #include <SFML/Graphics.hpp>
 
-float calculate_h_score(const sf::Vector2<unsigned short>& i_cell_0, const sf::Vector2<unsigned short>& i_cell_1);
+float calculate_h_score(const sf::Vector2<unsigned short>& i_cell0, const sf::Vector2<unsigned short>& i_cell1);
 
 void astar_reset(unsigned short& i_path_length, std::map<gbl::Position<>, gbl::Position<>>& i_previous_cell, std::vector<sf::Vector2<unsigned short>>& i_path_vector, gbl::MAP::Map<float>& i_f_scores, gbl::MAP::Map<float>& i_g_scores, gbl::MAP::Map<float>& i_h_scores, const sf::Vector2<unsigned short>& i_finish_position, const sf::Vector2<unsigned short>& i_start_position, gbl::MAP::Map<gbl::MAP::PathCell>& i_map);
 
