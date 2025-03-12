877b261cbd109bf1689374ec2251ea6421c20dad
skylee01244 <skylee01244@gmail.com>
2025-03-13 02:53:30 +1100

    Updated functions to implement namespace and easier readability

--- rpg-game/AngleFunctions.cpp
+++ rpg-game/AngleFunctions.cpp
@@ -4,34 +4,41 @@
 #include "Headers/AngleFunctions.h"
 #include "Headers/Global.h"
 
-float PI = 3.141592653589793116f;
-
-float normalize_degrees(float degrees)
-{
-    return fmod(360.0f + fmod(degrees, 360.0f), 360.0f);
+namespace {
+    constexpr float PI = 3.14159265358979323846f;
+    constexpr float CIRCLE = 360.0f;
 }
 
+
+// Normalize [0, 360)
+float normalize_degrees(float degrees)
+{
+    return std::fmod(CIRCLE + std::fmod(degrees, CIRCLE), CIRCLE);
+}
+// Normalize [0, 2pi)
 float normalize_radians(float radians)
 {
-    return fmod(2 * PI + fmod(radians, 2 * PI), 2 * PI);
+    return std::fmod(2.0f * PI + std::fmod(radians, 2.0f * PI), 2.0f * PI);
 }
 
 float degrees_to_radians(float degrees)
 {
-    return PI * normalize_degrees(degrees) / 180.0f;
+    return normalize_degrees(degrees) * (PI / 180.0f);
 }
 
 float radians_to_degrees(float radians)
 {
-    return 180.0f * normalize_radians(radians) / PI;
+    return normalize_radians(radians) * (180.0f / PI);
 }
 
 float degrees_difference(float angle1, float angle2)
 {
-    return 180.0f - fabs(fabs(normalize_degrees(angle1) - normalize_degrees(angle2)) - 180.0f);
+    float diff = std::fabs(normalize_degrees(angle1) - normalize_degrees(angle2));
+    return std::fmin(diff, CIRCLE - diff);
 }
 
 float radians_difference(float angle1, float angle2)
 {
-    return PI - fabs(fabs(normalize_radians(angle1) - normalize_radians(angle2)) - PI);
+    float diff = std::fabs(normalize_radians(angle1) - normalize_radians(angle2));
+    return std::fmin(diff, 2.0f * PI - diff);
 }
