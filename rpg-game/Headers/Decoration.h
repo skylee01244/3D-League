6d96b0b7f15719ca2c2cdbac1bc98d8ac3122a70
skylee01244 <skylee01244@gmail.com>
2025-03-14 10:20:02 +1100

    Updated variable names

--- rpg-game/Headers/Decoration.h
+++ rpg-game/Headers/Decoration.h
@@ -3,20 +3,14 @@
 
 class Decoration
 {
-	bool in_the_view;
-	//I didn't wanna make another class for animated decorations.
+	bool in_view;
 	bool is_animation;
-
 	float distance;
-
 	int screen_x;
 
 	std::string sprite_name;
-
 	sf::Vector2f position;
-
 	SpriteManager* sprite_manager;
-
 	Animation animation;
 public:
 	Decoration(const bool i_is_animation, const std::string& i_sprite_name, SpriteManager& i_sprite_manager, const float i_x = 0, const float i_y = 0);
@@ -25,7 +19,6 @@ public:
 	bool operator<(const Decoration& i_decoration) const;
 
 	float get_distance() const;
-
 	int get_height() const;
 	int get_width() const;
 	int get_x() const;
