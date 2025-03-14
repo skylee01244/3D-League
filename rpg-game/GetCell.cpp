f7611acfdad4a093e360a8d0d80de2902ddd9316
skylee01244 <skylee01244@gmail.com>
2025-03-14 16:06:08 +1100

    ADDED DEBUG

--- rpg-game/GetCell.cpp
+++ rpg-game/GetCell.cpp
@@ -11,5 +11,9 @@ gbl::MAP::PathCell get_cell(const sf::Vector2<short>& i_cell, const gbl::MAP::Ma
 		return i_map[i_cell.x][i_cell.y];
 	}
 
+#ifdef DEBUG
+	std::cerr << "[Warning] Attempted to access an out-of-bounds cell: ("<< cellPos.x << ", " << cellPos.y << ")\n";
+#endif
+
 	return gbl::MAP::PathCell::Invalid;
 }
